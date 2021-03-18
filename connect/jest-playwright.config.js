bb2e6577ed32401b82d3ac2ed8605d3d14c20a43
Zach Ferland <zachferland@gmail.com>
2021-03-17 20:04:35 -0400

    fix: unit test env fix, localhost integration test fix, change create prompt

--- connect/jest-playwright.config.js
+++ connect/jest-playwright.config.js
@@ -4,7 +4,7 @@ module.exports = {
     headless: true,
   },
   serverOptions: {
-    command: 'npm run test:ceramic',
+    command: 'npm run test:ceramic | npm run test:app',
     debug: true,
     launchTimeout: 60000,
     protocol: 'tcp',
--- connect/src/__tests__/manage3IDs.test.ts
+++ connect/src/__tests__/manage3IDs.test.ts
@@ -6,7 +6,6 @@ import { entropyToMnemonic } from '@ethersproject/hdnode'
 import { EthereumMigrationMockAuthProvider, createEthereumAuthProvider } from '../../test/utils'
 import { AuthProvider } from '../index'
 import { AuthProviderClient, AuthProviderServer } from '../authProviderRelay'
-import { serveCrossOrigin, serveSameOrigin } from '@ceramicnetwork/rpc-postmessage'
 import { authenticate } from '@ceramicnetwork/blockchain-utils-linking/lib/ethereum'
 
 let ceramic: Ceramic
@@ -27,6 +26,7 @@ beforeAll(async () => {
 })
 
 describe('3ID Management', () => {
+  jest.setTimeout(30000)
   test('initializes 3ID Managment', async () => {
     // auth provider create
     const authProvider = await createEthereumAuthProvider()
@@ -58,8 +58,6 @@ describe('3ID Management', () => {
 
   test('creates/loads did from in memory', async () => {})
 
-  //TODO This test fails, if you log the authclient and authserver you can see it returns
-  // unfefined to client before server returns response (or appears that way)
   test('creates/loads did with a RPC authprovider ', async () => {
     const ethAuthProvider = await createAuthProvider(6)
     const server = AuthProviderServer(ethAuthProvider)
@@ -68,7 +66,6 @@ describe('3ID Management', () => {
     const accountId = (await authProvider.accountId()).toString()
     const manage3ids = new Manage3IDs(authProvider, { ceramic })
     const did = await manage3ids.createAccount()
-    // expect link to be created
     const links = await idx.get('cryptoAccounts', did)
     expect(links[accountId]).toBeTruthy()
 
