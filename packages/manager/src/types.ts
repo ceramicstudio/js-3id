34748659d90f5033b1a4076aa95b1ba3d9d2b214
Zach Ferland <zachferland@gmail.com>
2021-05-27 21:34:23 -0400

    fix: deterministic by seed option

--- packages/manager/src/types.ts
+++ packages/manager/src/types.ts
@@ -1,4 +1,4 @@
 export type ExcludesBoolean = <T>(x: T | null) => x is T
 
 export type AuthConfig = { authId: string; authSecret: Uint8Array }
-export type SeedConfig = { v03ID: string; seed: Uint8Array }
+export type SeedConfig = { v03ID?: string; seed: Uint8Array, did?:string }
