4644e06f485ae5fb3c7ef1a9e59ad83d7a138927
Zach Ferland <zachferland@gmail.com>
2021-08-03 14:31:28 -0400

    feat: ui provider

---
+++ packages/ui-provider/src/types.ts
@@ -0,0 +1,53 @@
+import type { 
+  RPCClient,
+  RPCConnection, 
+  RPCRequest, 
+  RPCResponse, 
+  RPCErrorObject 
+} from 'rpc-utils'
+
+export type MigrationParams = {
+  legacyDid: string
+}
+
+export type MigrationRes = {
+  migration: boolean
+}
+
+export type MigrationSkipRes  = {
+  skip: boolean
+}
+
+export type AccountRes = {
+  createNew: boolean
+}
+
+export type AuthParams = {
+	origin: string
+  paths: Array<string> 
+  did: string
+}
+
+export type AuthRes = {
+  allow: boolean
+}
+
+export type UIProviderMethods = {
+  prompt_migration: { params: MigrationParams; result: MigrationRes }
+  prompt_migration_skip: { params: {}; result: MigrationSkipRes }
+  prompt_account: { params: {}; result: AccountRes }
+  prompt_authenticate: { params: AuthParams; result: AuthRes }
+  inform_error: { params: RPCErrorObject, result: null }
+}
+
+export type UIMethodName = keyof UIProviderMethods
+export type UIRequest<K extends UIMethodName = UIMethodName> = RPCRequest<UIProviderMethods, K>
+export type UIResponse<K extends UIMethodName = UIMethodName> = RPCResponse<
+  UIProviderMethods,
+  K
+>
+export type UIProviderInterface = RPCConnection<UIProviderMethods>
+export type UIProviderClient = RPCClient<UIProviderMethods>
+export type UIProviderOrClient = UIProviderInterface | UIProviderClient
+
+
