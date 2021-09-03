07f6aabc18729c5526a6c7d46e92e35894f4d3b1
Elizabeth Murray <elizabeth@3box.io>
2021-09-03 12:48:34 -0400

    remove self.id-connect package

--- packages/self.id-connect/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['3box', '3box/react', '3box/typescript'],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-  },
-}
--- packages/self.id-connect/src/errors/rpc.ts
+++
@@ -1,8 +0,0 @@
-import { RPCError } from 'rpc-utils'
-import type { RPCErrorObject } from 'rpc-utils'
-
-// TODO didprovider, auth failed codes?
-export const rpcError = (id: string | number): RPCErrorObject & { id: string | number } => {
-  const rpcError = new RPCError(-32401, `3id-connect: Request not authorized`)
-  return Object.assign(rpcError.toObject(), { id })
-}
