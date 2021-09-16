1ea81f233944d4067d7ddc036019b61730f2c8fd
Elizabeth Murray <elizabeth@3box.io>
2021-09-16 19:12:52 -0400

    initial connection to RPC Display & basic wallet communication

--- apps/3id-ui/src/Components/App/App.tsx
+++ apps/3id-ui/src/Components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { ConnectService } from '../../services/connectService'
 
@@ -6,32 +6,52 @@ import { UIProvider } from '@3id/ui-provider'
 import type { UIProviderHandlers } from '@3id/ui-provider'
 
 function App() {
+  //@ts-ignore
+  const render = async (params: object, type: string) => {
+    const result = await accepted()
+    return result
+  }
+
+  /**
+   * Connect methods
+   */
   const connectService = new ConnectService()
-  connectService.displayIframe()
 
   const UIMethods: UIProviderHandlers = {
     //@ts-ignore
     prompt_migration: async (_ctx = {}, params) => {
-      // should return { boolean }
+      await connectService.displayIframe()
+      const accept = await render(params, 'migration')
+      return { accept }
     },
     //@ts-ignore
     prompt_migration_skip: async (_ctx = {}, params) => {
-      // should return { boolean }
+      await connectService.displayIframe()
+      const accept = await render(params, 'migration_skip')
+      return { accept }
     },
     //@ts-ignore
     prompt_migration_fail: async (_ctx = {}, params) => {
-      // should return { boolean }
+      await connectService.displayIframe()
+      const accept = await render(params, 'migration_fail')
+      return { accept }
     },
     //@ts-ignore
     prompt_account: async (_ctx = {}, params) => {
-      // should return { boolean }
+      await connectService.displayIframe()
+      const accept = await render(params, 'account')
+      return { accept }
     },
     //@ts-ignore
     prompt_authenticate: async (_ctx = {}, params) => {
-      // should return { boolean }
+      await connectService.displayIframe()
+      const accept = await render(params, 'authenticate')
+      return { accept }
     },
     //@ts-ignore
     inform_error: async (_ctx = {}, params) => {
+      await connectService.displayIframe()
+      console.log('error')
       // TODO: error component here
       // if (params.data) {
       //   console.log(params.data.toString())
@@ -41,21 +61,27 @@ function App() {
   }
 
   const provider = new UIProvider(UIMethods)
+  let closecallback
+  const closing = (cb: any) => {
+    closecallback = cb
+  }
+  useEffect(() => {
+    connectService.start(provider, closing)
+  }, [])
+
+  const accepted = (): Promise<boolean> => {
+    return Promise.resolve(true)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div
+        className="btn"
+        onClick={() => {
+          accepted()
+        }}>
+        Accept
+      </div>
     </div>
   )
 }
