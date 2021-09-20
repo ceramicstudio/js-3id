451b5ee67f6e2b3d54650981388cafaa08004606
Elizabeth Murray <elizabeth@3box.io>
2021-09-20 14:44:24 -0400

    remove comments

--- apps/3id-ui/src/Components/App/App.tsx
+++ apps/3id-ui/src/Components/App/App.tsx
@@ -7,101 +7,11 @@ import { UIProvider } from '@3id/ui-provider'
 import type { UIProviderHandlers } from '@3id/ui-provider'
 
 type AppProps = {
-  request?: object
   buttons?: any
 }
-//@ts-ignore
-function App({ request, buttons }: AppProps) {
-  console.log('buttons', buttons?.acceptNode)
-  // const [acceptConnection, setAcceptConnection]: any = React.useState()
-  // /**
-  //  * Connect methods
-  //  */
-  // const connectService = new ConnectService()
 
-  // const render = async (params: object, type: string) => {
-  //   const request = Object.assign({}, params, { type })
-  // }
-
-  // const modalView = async (params: object, type: string) => {
-  //   await connectService.displayIframe()
-  //   await render(params, type)
-  // }
-
-  // const returnAcceptConnection = (): Promise<boolean> => {
-  //   return new Promise((resolve) => resolve(acceptConnection))
-  // }
-
-  // const UIMethods: UIProviderHandlers = {
-  //   //@ts-ignore
-  //   prompt_migration: async (_ctx = {}, params) => {
-  //     // this is not _just_ displaying the iframe, this also opens up the RPC connection
-
-  //     //@ts-ignore
-  //     await connectService.displayIframe(returnAcceptConnection)
-  //     return { acceptConnection }
-  //   },
-  //   //@ts-ignore
-  //   prompt_migration_skip: async (_ctx = {}, params) => {
-  //     //@ts-ignore
-  //     await connectService.displayIframe(returnAcceptConnection)
-  //     return { acceptConnection }
-  //   },
-  //   //@ts-ignore
-  //   prompt_migration_fail: async (_ctx = {}, params) => {
-  //     //@ts-ignore
-  //     await connectService.displayIframe(returnAcceptConnection)
-  //     return { acceptConnection }
-  //   },
-  //   //@ts-ignore
-  //   prompt_account: async (_ctx = {}, params) => {
-  //     //@ts-ignore
-  //     await connectService.displayIframe(returnAcceptConnection)
-  //     return { acceptConnection }
-  //   },
-  //   //@ts-ignore
-  //   prompt_authenticate: async (_ctx = {}, params) => {
-  //     //@ts-ignore
-  //     await connectService.displayIframe(returnAcceptConnection)
-  //     return { acceptConnection }
-  //   },
-  //   //@ts-ignore
-  //   inform_error: async (_ctx = {}, params) => {
-  //     console.log(params)
-  //     // TODO: error component here
-  //     // if (params.data) {
-  //     //   console.log(params.data.toString())
-  //     // }
-  //     // document.getElementById('action').innerHTML = error('Error: Unable to connect')
-  //   },
-  // }
-
-  // const provider = new UIProvider(UIMethods)
-  // let closecallback
-  // const closing = (cb: any) => {
-  //   closecallback = cb
-  // }
-
-  // React.useEffect(() => {
-  //   connectService.start(provider, closing)
-  // }, [])
-
-  // const accepted = (value: boolean) => {
-  //   setAcceptConnection(value)
-  // }
-
-  return (
-    <div className="App">
-      {buttons.acceptNode}
-      {/* <div
-        className="btn"
-        onClick={() => {
-          console.log('accept button')
-        }}>
-        Accept
-      </div> */}
-    </div>
-  )
+function App({ buttons }: AppProps) {
+  return <div className="App">{buttons.acceptNode}</div>
 }
 
 export default App
--- apps/3id-ui/src/index.tsx
+++ apps/3id-ui/src/index.tsx
@@ -12,7 +12,7 @@ const render = async (params: object, type: string, buttons: object) => {
   const request = Object.assign({}, params, { type })
   ReactDOM.render(
     <React.StrictMode>
-      <App request={request} buttons={buttons} />
+      <App buttons={buttons} />
     </React.StrictMode>,
     document.getElementById('root')
   )
@@ -22,31 +22,36 @@ const connectService = new ConnectService()
 
 const modalView = async (params: object, type: string) => {
   await connectService.displayIframe()
-  let acceptNode = <div className="btn">accept</div>
-  // let declineNode = <div className = "btn">decline</div>
+  let acceptNode = <div className="btn">Accept</div>
+  let declineNode = <div className="btn">Decline</div>
 
   const accepted = new Promise((resolve) => {
-    // acceptNode?.addEventListener('click', () => {
-    //   resolve(true)
-    // })
     acceptNode = (
       <div
         className="btn"
         onClick={() => {
           resolve(true)
         }}>
-        accept
+        Accept
       </div>
     )
-    // if (declineNode) {
-    //   resolve(false)
-    // }
+    if (declineNode) {
+      declineNode = (
+        <div
+          className="btn"
+          onClick={() => {
+            resolve(true)
+          }}>
+          Decline
+        </div>
+      )
+    }
   })
-  await render(params, type, { acceptNode /*, declineNode*/ })
+  await render(params, type, { acceptNode, declineNode })
   return {
     accepted,
     acceptNode,
-    // declineNode,
+    declineNode,
   }
 }
 
@@ -54,11 +59,6 @@ const UIMethods: UIProviderHandlers = {
   //@ts-ignore
   prompt_migration: async (ctx = {}, params: object) => {
     const modal = await modalView(params, 'migration')
-    //@ts-ignore
-    // modal.acceptNode.addEventListener('click', () => {
-    //   //@ts-ignore
-    //   modal.acceptNode.innerHTML = `Migrating`
-    // })
     const migration = await modal.accepted
     console.log(migration)
     return { migration }
@@ -66,47 +66,29 @@ const UIMethods: UIProviderHandlers = {
   //@ts-ignore
   prompt_migration_skip: async (ctx = {}, params: object) => {
     const modal = await modalView(params, 'migration_skip')
-    //@ts-ignore
-    modal.acceptNode.addEventListener('click', () => {
-      //@ts-ignore
-      modal.acceptNode.innerHTML = `Creating account `
-    })
     const skip = await modal.accepted
     return { skip }
   },
   //@ts-ignore
   prompt_migration_fail: async (ctx = {}, params: object) => {
     const modal = await modalView(params, 'migration_fail')
-    //@ts-ignore
-    modal.acceptNode.addEventListener('click', () => {
-      //@ts-ignore
-      modal.acceptNode.innerHTML = `Creating account `
-    })
     const createNew = await modal.accepted
     return { createNew }
   },
   //@ts-ignore
   prompt_account: async (ctx = {}, params: object) => {
     const modal = await modalView(params, 'account')
-    // modal.declineNode.addEventListener('click', () => {
-    //   modal.declineNode.innerHTML = `Creating account `
-    //   modal.declineNode.style.boxShadow = 'none'
-    // })
     const createNew = !(await modal.accepted)
     return { createNew }
   },
   //@ts-ignore
   prompt_authenticate: async (ctx = {}, params: object) => {
     const modal = await modalView(params, 'authenticate')
-    //@ts-ignore
-    // modal.acceptNode.addEventListener('click', () => {
-    //   //@ts-ignore
-    //   modal.acceptNode.innerHTML = `Continue `
-    // })
     const allow = await modal.accepted
     console.log(allow)
     return { allow }
   },
+  // // TODO: reenable this
   // inform_error: async (ctx = {}, params: object) => {
   //   if (params.data) {
   //     console.log(params.data.toString())
@@ -118,7 +100,7 @@ const UIMethods: UIProviderHandlers = {
 //Create a 3ID Connect UI Provider
 const provider = new UIProvider(UIMethods)
 
-// Closure to pass cancel state to IDW iframe service, TODO
+// Closure to pass cancel state to IDW iframe service
 let closecallback: any
 
 // @ts-ignore
@@ -135,14 +117,4 @@ const closing = (cb: any) => {
 
 connectService.start(provider, closing)
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
