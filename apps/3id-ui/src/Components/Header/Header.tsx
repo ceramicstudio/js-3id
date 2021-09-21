22c53129b1925c137ed715cd3b6bba95b6e2b713
Elizabeth Murray <elizabeth@3box.io>
2021-09-21 15:19:26 -0400

    first pass of reconciling old ui with new structure

---
+++ apps/3id-ui/src/Components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+import Avatar from 'boring-avatars'
+
+import './Header.scss'
+
+/**
+ * Handle all the data updates that are needed for display.
+ */
+export const Header = () => {
+  // Get the Favicon of the parent window
+  // const referrerUrl =
+  //   window.location != window.parent.location ? document.referrer : document.location.href
+  // Fetch the page & snag the favicon
+
+  const did = undefined // user?.id // TODO: Set based off of IDX DID
+  return (
+    <div className="head">
+      <div className="logo-container">
+        <a
+          href="https://ceramic.network"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="logo col-12">
+          SelfId Connect
+        </a>
+        <span> SelfID Connect </span>
+      </div>
+      <div className="image-container">
+        <div className="avatar">
+          <Avatar
+            size={65}
+            name={did ?? 'self.id-connect'}
+            variant="marble"
+            colors={['#FF0092', '#FFCA1B', '#B6FF00', '#228DFF', '#BA01FF']}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
