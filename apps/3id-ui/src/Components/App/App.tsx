82161b26bd52a20f9b208a4218d8302c3aa92111
Elizabeth Murray <elizabeth@3box.io>
2021-09-22 13:10:27 -0400

    reconcile all previous UI changes

--- apps/3id-ui/src/Components/App/App.tsx
+++ apps/3id-ui/src/Components/App/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import './App.css'
-
+import './App.scss'
 import Modal from '../Modal/Modal'
+
+import * as hexToRgb from 'hex-to-rgb'
 
 type AppProps = {
   buttons: any
   request: any
 }
 
+const hexToRGBA = (hex: string, opacity?: number | null): string =>
+  `rgba(${hexToRgb(hex) as string}, ${(opacity || 30) / 100})`
+
 const App = ({ buttons, request }: AppProps) => {
-  return <Modal buttons={buttons} request={request} />
+  return (
+    <div
+      className="App"
+      // TODO: dynamically set this based off of request object.
+      style={{ backgroundColor: `${hexToRGBA('#e4e4e4')}` }}>
+      <Modal buttons={buttons} request={request} />
+    </div>
+  )
 }
 
 export default App
