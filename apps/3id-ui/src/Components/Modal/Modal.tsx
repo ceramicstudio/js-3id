057bd9f0bb4c017d3a26e97f5420fe726e061357
Zach Ferland <zachferland@gmail.com>
2021-10-06 13:58:30 -0400

    fix(3id-ui): modal layout, migration error message

--- apps/3id-ui/src/Components/Modal/Modal.tsx
+++ apps/3id-ui/src/Components/Modal/Modal.tsx
@@ -63,7 +63,6 @@ export const Modal = ({ request, buttons }: ModalProps) => {
             {permissionDisplay}
           </div>
           <div className="bottom">
-            hi
             {acceptNode}
             {declineNode}
           </div>
@@ -78,6 +77,23 @@ export const Modal = ({ request, buttons }: ModalProps) => {
             <br />
             <a
               href="developers.ceramic.network/authentication/legacy/3id-connect-migration"
+              rel="noopener noreferrer"
+              target="_blank">
+              Learn More
+            </a>
+          </div>
+          <div className="bottom">{acceptNode}</div>
+        </>
+      )
+    } else if (type === 'migration_fail') {
+      body = (
+        <>
+          <div>
+            Your 3Box account could not be migrated, continue with a new account?
+            <br />
+            <br />
+            <a
+              href="https://developers.ceramic.network/authentication/legacy/3id-connect-migration"
               rel="noopener noreferrer"
               target="_blank">
               Learn More
@@ -107,8 +123,6 @@ export const Modal = ({ request, buttons }: ModalProps) => {
     }
     return body
   }
-
-  console.log(closeNode)
   return (
     <div className="modal">
       <Header closeButton={closeNode} />
