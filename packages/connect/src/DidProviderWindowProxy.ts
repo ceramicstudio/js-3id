2b288ca76e0103b480f0f898dfde4c1ba2824a2c
Zach Ferland <zachferland@gmail.com>
2022-03-01 13:56:43 -0500

    Ref/remove postmsg (#224)
    * ref: remove post msg rpc
    
    * fix(service): authprovider relay instance
    
    * ref: move didprov proxy to connect, remove from common
    
    Co-authored-by: Paul Le Cam <paul@3box.io>

---
+++ packages/connect/src/DidProviderWindowProxy.ts
@@ -0,0 +1,22 @@
+import type { DIDMethodName, DIDProvider, DIDProviderMethods } from 'dids'
+import { RPCClient, RPCResponse, RPCRequest } from 'rpc-utils'
+import { createClient } from '@ceramicnetwork/rpc-window'
+import { DIDRPCNameSpace } from '@3id/common'
+
+export class DidProviderWindowProxy implements DIDProvider {
+  _client: RPCClient<DIDProviderMethods>
+
+  constructor(target: Window, namespace = DIDRPCNameSpace) {
+    this._client = createClient<DIDProviderMethods>(namespace, target)
+  }
+
+  get isDidProvider(): boolean {
+    return true
+  }
+
+  async send<Name extends DIDMethodName>(
+    msg: RPCRequest<DIDProviderMethods, Name>
+  ): Promise<RPCResponse<DIDProviderMethods, Name> | null> {
+    return this._client.connection.send(msg)
+  }
+}
--- packages/connect/src/index.ts
+++ packages/connect/src/index.ts
@@ -1,7 +1,5 @@
 // Re-exports supported (tested) providers from blockchain-utils-linking
 export { EthereumAuthProvider } from '@ceramicnetwork/blockchain-utils-linking'
 export type { AuthProvider } from '@ceramicnetwork/blockchain-utils-linking'
-
-export { DidProviderProxy } from './didProviderProxy'
 export { ThreeIdConnect } from './threeIdConnect'
 export * from './types'
